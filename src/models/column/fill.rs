a1be9c8a68db32a6186c5b80b1927867cbd21f8f
mertkanakkoc <mertkan_240@hotmail.com>
2026-08-16 20:05:25 +0300

    feat: fill_with and helper function added
    filling missing elements with the value given added + find indices of
    missing elements added

--- src/models/column/fill.rs
+++ src/models/column/fill.rs
@@ -1,1 +1,29 @@
+use crate::models::column::ColumnError;
+
 use super::Column;
+
+impl<T> Column<T> {
+    pub fn missing_indices(&self) -> Vec<usize> {
+        self.data
+            .iter()
+            .enumerate()
+            .filter_map(|(i, val)| val.is_none().then_some(i))
+            .collect()
+    }
+
+    pub fn fill_with(&mut self, value: T) -> Result<String, ColumnError>
+    where
+        T: Clone,
+    {
+        let missing_indices = self.missing_indices();
+        let new_elements = missing_indices
+            .into_iter()
+            .map(|x| (x, Some(value.clone())))
+            .collect();
+        let update_result = self.update_element(new_elements);
+        match update_result {
+            Err(e) => return Err(e),
+            _ => Ok("Missing elements were filled.".to_string()),
+        }
+    }
+}
