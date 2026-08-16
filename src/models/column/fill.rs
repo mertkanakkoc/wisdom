429e5323db5c94e7edbf54d0b805d5ae7fc84cfe
mertkanakkoc <mertkan_240@hotmail.com>
2026-08-16 20:18:10 +0300

    test: test functions added for missing_indices and fill_with functions

--- src/models/column/fill.rs
+++ src/models/column/fill.rs
@@ -27,3 +27,49 @@ impl<T> Column<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn missing_indices_returns_indices_of_none_values() {
+        let column =
+            Column::new_from_parsed(vec![Some(1), None, Some(3), None], "age".to_string()).unwrap();
+
+        assert_eq!(column.missing_indices(), vec![1, 3])
+    }
+
+    #[test]
+    fn missing_indices_returns_empty_when_none_missing() {
+        let column = Column::new_from_parsed(vec![Some(1), Some(2)], "age".to_string()).unwrap();
+
+        assert_eq!(column.missing_indices(), vec![])
+    }
+
+    #[test]
+    fn fill_with_replaces_missing_values() {
+        let mut column =
+            Column::new_from_parsed(vec![Some(1), None, Some(3), None], "age".to_string()).unwrap();
+
+        let result = column.fill_with(0);
+
+        assert!(result.is_ok());
+        assert_eq!(column.get(0), Some(&Some(1)));
+        assert_eq!(column.get(1), Some(&Some(0)));
+        assert_eq!(column.get(2), Some(&Some(3)));
+        assert_eq!(column.get(3), Some(&Some(0)));
+    }
+
+    #[test]
+    fn fill_with_is_noop_when_nothing_missing() {
+        let mut column =
+            Column::new_from_parsed(vec![Some(1), Some(2)], "age".to_string()).unwrap();
+
+        let result = column.fill_with(99);
+
+        assert!(result.is_ok());
+        assert_eq!(column.get(0), Some(&Some(1)));
+        assert_eq!(column.get(1), Some(&Some(2)));
+    }
+}
