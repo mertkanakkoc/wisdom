686bd7f05b2236cb2e8599ac8ba646ead0c62e9f
mertkanakkoc <mertkan_240@hotmail.com>
2026-08-31 12:37:15 +0300

    feat: creating struct to hold output of a training session

--- src/ml/training/artifact.rs
+++ src/ml/training/artifact.rs
@@ -1,6 +1,14 @@
+use candle_nn::VarMap;
+
 pub enum ModelArchitecture {
     Linear {
         in_features: usize,
         out_features: usize,
     },
+}
+
+pub struct TrainingOutput<M> {
+    model: M,
+    varmap: VarMap,
+    architecture: ModelArchitecture,
 }
